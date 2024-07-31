3d31cd3a002803250a88711294d0a64b6da7c32e
Ajay <misdeptho@gmail.com>
2024-07-31 17:40:27 -0400

    penalty intime

--- src/screens/admin/penalty.js
+++ src/screens/admin/penalty.js
@@ -1,19 +1,81 @@
-// screens/PenaltyScreen.js
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, TextInput, Button} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, TextInput, Button, Alert } from 'react-native';
 import DateTimePickerModal from 'react-native-modal-datetime-picker';
 import { Picker } from '@react-native-picker/picker';
-import moment from 'moment';
+import moment from 'moment-timezone';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { Table, Row, Rows } from 'react-native-table-component';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios'; // Ensure axios is installed
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { ScrollView } from 'react-native-gesture-handler';
 
-const Penalty = () => {
+const Penalty = ({ navigation }) => {
   const [fromDate, setFromDate] = useState(null);
   const [toDate, setToDate] = useState(null);
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
   const [dateMode, setDateMode] = useState('from'); // 'from' or 'to'
   const [reportType, setReportType] = useState('dailywise');
-  const [employeeId, setEmployeeId] = useState('');
+  const [EmployeeId, setEmployeeId] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState(null);
+  const [data, setData] = useState([]);
+  const [tokenFactoryId, setTokenFactoryId] = useState(null);
+  const [employeeName, setEmployeeName] = useState('');
+
+  useEffect(() => {
+    checkAuthentication();
+  }, []);
+
+  useEffect(() => {
+    if (token) {
+      fetchUserDetails(token);
+    }
+  }, [token]);
+
+  const checkAuthentication = async () => {
+    try {
+      const storedToken = await AsyncStorage.getItem('EmployeeId');
+      const storedFactoryId = await AsyncStorage.getItem('FactoryId');
+
+      if (!storedToken || !storedFactoryId) {
+        console.log('User is not authenticated. Redirecting to login screen...');
+        navigation.navigate('Login');
+      } else {
+        console.log('User is authenticated.');
+        setIsLoggedIn(true);
+        setToken(storedToken); // Assuming you need to use EmployeeId as token or for display purposes.
+        setTokenFactoryId(storedFactoryId); // Assuming you need to use FactoryId.
+        setEmployeeId(storedToken); // Set the EmployeeId state
+      }
+    } catch (error) {
+      console.error('Error checking authentication:', error.message);
+    }
+  };
+
+  const fetchUserDetails = async token => {
+    try {
+      const EmployeeId = await AsyncStorage.getItem('EmployeeId');
+
+      const response = await axios.get(
+        `http://10.0.2.2:3000/api/v2/lve/employeId/${EmployeeId}`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      console.log('User Response:', response.data);
+      if (response.data.length > 0) {
+        setEmployeeName(response.data[0].Employee);
+      } else {
+        console.error('User details retrieval failed:', response.data.message);
+      }
+    } catch (error) {
+      console.error('Error fetching user details:', error.message);
+    }
+  };
 
   const showDatePicker = (mode) => {
     setDateMode(mode);
@@ -33,70 +95,115 @@ const Penalty = () => {
     hideDatePicker();
   };
 
-  const handleSubmit = () => {
-    // Implement your submit logic here
-    console.log('Submitted');
+  const handleSubmit = async () => {
+    if (!fromDate || !toDate || !EmployeeId) {
+      Alert.alert('Error', 'Please fill all fields');
+      return;
+    }
+  
+    const formattedFromDate = moment(fromDate).format('YYYY-MM-DD');
+    const formattedToDate = moment(toDate).format('YYYY-MM-DD');
+  
+    console.log('FromDate:', formattedFromDate);
+    console.log('ToDate:', formattedToDate);
+    console.log('EmployeeId:', EmployeeId);
+  
+    try {
+      const response = await axios.get(`http://10.0.2.2:3000/api/v2/pen/penalty/${EmployeeId}`, {
+        params: {
+          FromDate: formattedFromDate,
+          ToDate: formattedToDate,
+        },
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+  
+      setData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error.response ? error.response.data : error.message);
+      Alert.alert('Error', 'Failed to fetch data');
+    }
   };
 
-  const tableHead = ['ECNO', 'Date', 'Employee Name', 'Intime', 'Late Mins', 'Amount'];
-  const tableData = [
-    ['001', '12/07/2024', 'John Doe', '09:10', '10', '$5'],
-    ['002', '12/07/2024', 'Jane Smith', '09:15', '15', '$7.5'],
-    ['003', '12/07/2024', 'Robert Brown', '09:05', '5', '$2.5'],
-  ];
+  const tableHead = [ 'Date', 'Intime', 'Late Mins', 'Amount'];
 
+  const formatTableData = (data) => {
+    return data.map((item) => {
+      let formattedInTime = '';
+  
+      if (item.InTime) {
+        try {
+          // Ensure the InTime is treated as UTC before converting to local time
+          formattedInTime = moment.utc(item.InTime).format('HH:mm'); // Convert to local time
+        } catch (error) {
+          console.error('Error formatting InTime:', error);
+          formattedInTime = 'Invalid Time';
+        }
+      }
+  
+      return [
+        moment(item.AttDate).format('DD/MM/YYYY'),
+        formattedInTime,
+        item.LateMins,
+        item.Amount,
+      ];
+    });
+  };
+  
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>Late/Early Penalty Report</Text>
-      </View>
-      <View style={styles.filters}>
-        <View style={styles.datePickerContainer}>
-          <TouchableOpacity onPress={() => showDatePicker('from')} style={styles.dateInput}>
-            <Icon name="calendar" size={20} color="#666" style={styles.icon} />
-            <Text style={styles.dateText}>{fromDate ? moment(fromDate).format('DD/MM/YYYY') : 'From Date'}</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => showDatePicker('to')} style={styles.dateInput}>
-            <Icon name="calendar" size={20} color="#666" style={styles.icon} />
-            <Text style={styles.dateText}>{toDate ? moment(toDate).format('DD/MM/YYYY') : 'To Date'}</Text>
-          </TouchableOpacity>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Penalty Report</Text>
         </View>
-        <Picker
-          selectedValue={reportType}
-          style={styles.input}
-          onValueChange={(itemValue) => setReportType(itemValue)}
-        >
-          <Picker.Item label="Dailywise" value="dailywise" />
-          <Picker.Item label="Summary" value="summary" />
-          <Picker.Item label="Other" value="other" />
-        </Picker>
-        <TextInput
-          style={styles.input}
-          placeholder="Employee ID"
-          value={employeeId}
-          onChangeText={setEmployeeId}
+        <View style={styles.filters}>
+          <View style={styles.datePickerContainer}>
+            <TouchableOpacity onPress={() => showDatePicker('from')} style={styles.dateInput}>
+              <Icon name="calendar" size={20} color="#666" style={styles.icon} />
+              <Text style={styles.dateText}>{fromDate ? moment(fromDate).format('DD/MM/YYYY') : 'From Date'}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => showDatePicker('to')} style={styles.dateInput}>
+              <Icon name="calendar" size={20} color="#666" style={styles.icon} />
+              <Text style={styles.dateText}>{toDate ? moment(toDate).format('DD/MM/YYYY') : 'To Date'}</Text>
+            </TouchableOpacity>
+          </View>
+          <Picker
+            selectedValue={reportType}
+            style={styles.input}
+            onValueChange={(itemValue) => setReportType(itemValue)}
+          >
+            <Picker.Item label="Dailywise" value="dailywise" />
+            <Picker.Item label="Summary" value="summary" />
+            <Picker.Item label="Other" value="other" />
+          </Picker>
+          <TextInput
+            style={styles.input}
+            value={employeeName}
+            editable={false}
+          />
+          <Button title="Submit" onPress={handleSubmit} />
+        </View>
+        <View style={styles.tableContainer}>
+          <Text style={styles.text}>
+            Report from {fromDate ? moment(fromDate).format('DD/MM/YYYY') : 'N/A'} to {toDate ? moment(toDate).format('DD/MM/YYYY') : 'N/A'}
+          </Text>
+          <Text style={styles.text}>
+            Employee: {employeeName} 
+          </Text>
+          <Table borderStyle={{ borderWidth: 1, borderColor: '#ccc' }}>
+            <Row data={tableHead} style={styles.head} textStyle={styles.text} />
+            <Rows data={formatTableData(data)} textStyle={styles.text} />
+          </Table>
+        </View>
+        <DateTimePickerModal
+          isVisible={isDatePickerVisible}
+          mode="date"
+          onConfirm={handleConfirm}
+          onCancel={hideDatePicker}
         />
-        <Button title="Submit" onPress={handleSubmit} />
-      </View>
-      <View style={styles.tableContainer}>
-        <Text style={styles.reportText}>
-          Report from {fromDate ? moment(fromDate).format('DD/MM/YYYY') : 'N/A'} to {toDate ? moment(toDate).format('DD/MM/YYYY') : 'N/A'}
-        </Text>
-        <Text style={styles.reportText}>
-         employee : Ajay :1615
-        </Text>
-        <Table borderStyle={{ borderWidth: 1, borderColor: '#ccc' }}>
-          <Row data={tableHead} style={styles.head} textStyle={styles.text} />
-          <Rows data={tableData} textStyle={styles.text} />
-        </Table>
-      </View>
-      <DateTimePickerModal
-        isVisible={isDatePickerVisible}
-        mode="date"
-        onConfirm={handleConfirm}
-        onCancel={hideDatePicker}
-      />
-    </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
@@ -134,40 +241,42 @@ const styles = StyleSheet.create({
   dateInput: {
     flexDirection: 'row',
     alignItems: 'center',
-    borderBottomWidth: 1,
+    backgroundColor: '#f9f9f9',
+    padding: 10,
+    borderRadius: 5,
+    borderWidth: 1,
     borderColor: '#ccc',
-    paddingVertical: 10,
-    marginBottom: 10,
-    flex: 0.48,
+    marginVertical: 5,
+    width: '48%',
   },
   icon: {
     marginRight: 10,
   },
   dateText: {
-    color: '#666',
-  },
-  input: {
-    borderBottomWidth: 1,
-    borderColor: '#ccc',
-    paddingVertical: 10,
-    marginBottom: 10,
-  },
-  tableContainer: {
-    marginTop: 20,
-  },
-  reportText: {
-    textAlign: 'center',
-    marginBottom: 10,
     fontSize: 16,
     color: '#666',
   },
+  input: {
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginVertical: 5,
+    paddingHorizontal: 10,
+    backgroundColor: '#f9f9f9',
+     color: '#666'
+  },
+  tableContainer: {
+    marginBottom: 20,
+  },
   head: {
-    height: 89,
+    height: 40,
     backgroundColor: '#f1f8ff',
   },
   text: {
     margin: 6,
     textAlign: 'center',
+     color: '#666'
   },
 });
 
--- src/screens/admin/profile.js
+++ src/screens/admin/profile.js
@@ -1,45 +1,40 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, TextInput, StyleSheet, Image, ScrollView, SafeAreaView} from 'react-native';
+import { View, Text, TextInput, StyleSheet, Image, ScrollView, SafeAreaView } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import axios from 'axios'
+import axios from 'axios';
 import AsyncStorage from "@react-native-async-storage/async-storage";
-
+import moment from 'moment';  // Import moment for date formatting
 
 const Profile = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [token, setToken] = useState(null);
-  const[profileDetails,setProfileDetails]=useState([])
+  const [profileDetails, setProfileDetails] = useState({});
 
   useEffect(() => {
     checkAuthentication();
   }, []);
+
   useEffect(() => {
     if (token) {
       fetchUserDetails(token);
-     
     }
   }, [token]);
+
   const checkAuthentication = async () => {
     try {
       const storedToken = await AsyncStorage.getItem('EmployeeId');
-     
-
       if (!storedToken) {
-        console.log(
-          'User is not authenticated. Redirecting to login screen...',
-        );
+        console.log('User is not authenticated. Redirecting to login screen...');
         Navigation.navigate('Login');
       } else {
         console.log('User is authenticated.');
         setIsLoggedIn(true);
         setToken(storedToken); // Assuming you need to use EmployeeId as token or for display purposes.
-        // Assuming you need to use FactoryId.
       }
     } catch (error) {
       console.error('Error checking authentication:', error.message);
     }
   };
-
 
   const fetchUserDetails = async (token) => {
     try {
@@ -57,164 +52,167 @@ const Profile = () => {
       console.error('Error fetching profile details:', error.message);
     }
   };
-  
+
+  const formatDate = (dateString) => {
+    return moment(dateString).format('DD/MM/YYYY');
+  };
 
   return (
     <SafeAreaView contentContainerStyle={styles.container}>
-    <ScrollView>
-      <Image source={require('../../assets/mini.jpg')} style={styles.profileImage} />
+      <ScrollView>
+        <Image source={require('../../assets/mini.jpg')} style={styles.profileImage} />
 
-      <View style={styles.infoContainer}>
-        <Text style={styles.label}>Biometric Code :</Text>
-        <Text style={styles.input}>{profileDetails.BiometricCode || ''}</Text>
-      </View>
-      <View style={styles.infoContainer}>
-        <Text style={styles.label}>Date of Birth :</Text>
-        <TextInput
-          style={styles.input}
-          value={profileDetails.DateofBirth || ''}
-          editable={false}
-        />
-      </View>
-      <View style={styles.infoContainer}>
-        <Text style={styles.label}>Date of Joining :</Text>
-        <TextInput
-          style={styles.input}
-          value={profileDetails.DateofJoining || ''}
-          editable={false}
-        />
-      </View>
-      <View style={styles.infoContainer}>
-        <Text style={styles.label}>Employee Name :</Text>
-        <Text style={styles.input}>
-          {profileDetails.Name || ''} / {profileDetails.DesignationName || ''}
-        </Text>
-      </View>
-      <View style={styles.infoContainer}>
-        <TextInput
-          style={styles.input}
-          value={profileDetails.MobileNo || ''}
-          editable={false}
-        />
-        <Icon name="phone" size={24} style={styles.icon} />
-      </View>
-      <View style={styles.infoContainer}>
-        <Text style={styles.label}>Father/Guardian Name :</Text>
-        <TextInput
-          style={styles.input}
-          value={profileDetails.FatherName || ''}
-          editable={false}
-        />
-      </View>
-      <View style={styles.infoContainer}>
-        <TextInput
-          style={styles.inputWithIcon}
-          value={profileDetails.alternatemobileno || ''}
-          editable={false}
-        />
-        <Icon name="phone" size={24} style={styles.icon} />
-      </View>
-      <View style={styles.infoContainer}>
-        <Text style={styles.label}>Spouse Name :</Text>
-        <TextInput
-          style={styles.input}
-          value={profileDetails.SpouseName || ''}
-          editable={false}
-        />
-      </View>
-      <View style={styles.infoContainer}>
-        <TextInput
-          style={styles.inputWithIcon}
-          value={profileDetails.SpouseMobileNo || ''}
-          editable={false}
-        />
-        <Icon name="phone" size={24} style={styles.icon} />
-      </View>
-      <View style={styles.infoContainer}>
-        <Text style={styles.label}>Mother Name :</Text>
-        <TextInput
-          style={styles.input}
-          value={profileDetails.MotherName || ''}
-          editable={false}
-        />
-      </View>
-      <View style={styles.infoContainer}>
-        <TextInput
-        style={styles.input}
-          
-          value={profileDetails.MotherMobileNo || ''}
-          editable={false}
-        />
-        <Icon name="phone" size={24} style={styles.icon} />
-      </View>
-    </ScrollView>
-  </SafeAreaView>
+        <View style={styles.infoContainer}>
+          <Text style={styles.label}>Biometric Code :</Text>
+          <Text style={styles.input}>{profileDetails.BiometricCode || ''}</Text>
+        </View>
+        <View style={styles.infoContainer}>
+          <Text style={styles.label}>Date of Birth :</Text>
+          <TextInput
+            style={styles.input}
+            value={profileDetails.DateofBirth ? formatDate(profileDetails.DateofBirth) : ''}
+            editable={false}
+          />
+        </View>
+        <View style={styles.infoContainer}>
+          <Text style={styles.label}>Date of Joining :</Text>
+          <TextInput
+            style={styles.input}
+            value={profileDetails.DateofJoining ? formatDate(profileDetails.DateofJoining) : ''}
+            editable={false}
+          />
+        </View>
+        <View style={styles.infoContainer}>
+          <Text style={styles.label}>Employee Name :</Text>
+          <Text style={styles.input}>
+            {profileDetails.Name || ''} / {profileDetails.DesignationName || ''}
+          </Text>
+        </View>
+        <View style={styles.infoContainer}>
+          <TextInput
+            style={styles.input}
+            value={profileDetails.MobileNo || ''}
+            editable={false}
+          />
+          <Icon name="phone" size={24} style={styles.icon} />
+        </View>
+        <View style={styles.infoContainer}>
+          <Text style={styles.label}>Father/Guardian Name :</Text>
+          <TextInput
+            style={styles.input}
+            value={profileDetails.FatherName || ''}
+            editable={false}
+          />
+        </View>
+        <View style={styles.infoContainer}>
+          <TextInput
+            style={styles.input}
+            value={profileDetails.alternatemobileno || ''}
+            editable={false}
+          />
+          <Icon name="phone" size={24} style={styles.icon} />
+        </View>
+        <View style={styles.infoContainer}>
+          <Text style={styles.label}>Spouse Name :</Text>
+          <TextInput
+            style={styles.input}
+            value={profileDetails.SpouseName || ''}
+            editable={false}
+          />
+        </View>
+        <View style={styles.infoContainer}>
+          <TextInput
+            style={styles.input}
+            value={profileDetails.SpouseMobileNo || ''}
+            editable={false}
+          />
+          <Icon name="phone" size={24} style={styles.icon} />
+        </View>
+        <View style={styles.infoContainer}>
+          <Text style={styles.label}>Mother Name :</Text>
+          <TextInput
+            style={styles.input}
+            value={profileDetails.MotherName || ''}
+            editable={false}
+          />
+        </View>
+        <View style={styles.infoContainer}>
+          <TextInput
+           style={styles.input}
+            value={profileDetails.MotherMobileNo || ''}
+            editable={false}
+          />
+          <Icon name="phone" size={24} style={styles.icon} />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
+
 const styles = StyleSheet.create({
-    container: {
-      flexGrow: 1,
-      padding: 16,
-      backgroundColor: '#fff',
-    },
-    profileImage: {
-      width: 100,
-      height: 100,
-      borderRadius: 75,
-      alignSelf: 'center',
-      marginBottom: 16,
-    },
-    infoContainer: {
-      marginBottom: 16,
-    },
-    label: {
-      fontSize: 16,
-      fontWeight: 'bold',
-      marginBottom: 4,
-    },
-    infoText: {
-      fontSize: 16,
-      marginBottom: 4,
-    },
-    input: {
-      borderWidth: 1,
-      borderColor: '#ccc',
-      borderRadius: 8,
-      paddingHorizontal: 12,
-      paddingVertical: 8,
-      fontSize: 16,
-      backgroundColor: '#f5f5f5',
-      color:'black'
-    },
-    inputWithIcon: {
-      borderWidth: 1,
-      borderColor: '#ccc',
-      borderRadius: 8,
-      paddingHorizontal: 12,
-      paddingVertical: 8,
-      fontSize: 16,
-      backgroundColor: '#f5f5f5',
-      marginBottom: 8,
-    },
-    icon: {
-      position: 'absolute',
-      top: 12,
-      right: 12,
-      color: '#888',
-    },
-    button: {
-      backgroundColor: '#007bff',
-      paddingVertical: 12,
-      paddingHorizontal: 16,
-      borderRadius: 8,
-      alignItems: 'center',
-      marginBottom: 16,
-    },
-    buttonText: {
-      color: '#fff',
-      fontSize: 16,
-      fontWeight: 'bold',
-    },
-  });
-  
-  export default Profile
+  container: {
+    flexGrow: 1,
+    padding: 16,
+    backgroundColor: '#fff',
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 75,
+    alignSelf: 'center',
+    marginBottom: 16,
+  },
+  infoContainer: {
+    marginBottom: 16,
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 4,
+  },
+  infoText: {
+    fontSize: 16,
+    marginBottom: 4,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    fontSize: 16,
+    backgroundColor: '#f5f5f5',
+    color: 'black',
+  },
+  inputWithIcon: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    fontSize: 16,
+    backgroundColor: '#f5f5f5',
+    marginBottom: 8,
+  },
+  icon: {
+    position: 'absolute',
+    top: 12,
+    right: 12,
+    color: '#888',
+  },
+  button: {
+    backgroundColor: '#007bff',
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+export default Profile;

d273df246aa059159c91698f477c5aabb7409e93
Ajay <misdeptho@gmail.com>
2024-08-01 15:49:12 -0400

    penalty report completed

--- src/screens/admin/penalty.js
+++ src/screens/admin/penalty.js
@@ -95,19 +95,81 @@ const Penalty = ({ navigation }) => {
     hideDatePicker();
   };
 
+  const handleSubmitOthers = async () => {
+    if (!fromDate || !toDate || !EmployeeId) {
+      Alert.alert('Error', 'Please fill all fields');
+      return;
+    }
+
+    const formattedFromDate = moment(fromDate).format('YYYY-MM-DD');
+    const formattedToDate = moment(toDate).format('YYYY-MM-DD');
+
+    console.log('FromDate:', formattedFromDate);
+    console.log('ToDate:', formattedToDate);
+    console.log('EmployeeId:', EmployeeId);
+
+    try {
+      const response = await axios.get(`http://10.0.2.2:3000/api/v2/pen/others/${EmployeeId}`, {
+        params: {
+          FromDate: formattedFromDate,
+          ToDate: formattedToDate,
+        },
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error.response ? error.response.data : error.message);
+      Alert.alert('Error', 'Failed to fetch data');
+    }
+  };
+
+  const handleSubmitSummery = async () => {
+    if (!fromDate || !toDate || !EmployeeId) {
+      Alert.alert('Error', 'Please fill all fields');
+      return;
+    }
+
+    const formattedFromDate = moment(fromDate).format('YYYY-MM-DD');
+    const formattedToDate = moment(toDate).format('YYYY-MM-DD');
+
+    console.log('FromDate:', formattedFromDate);
+    console.log('ToDate:', formattedToDate);
+    console.log('EmployeeId:', EmployeeId);
+
+    try {
+      const response = await axios.get(`http://10.0.2.2:3000/api/v2/pen/summery/${EmployeeId}`, {
+        params: {
+          FromDate: formattedFromDate,
+          ToDate: formattedToDate,
+        },
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error.response ? error.response.data : error.message);
+      Alert.alert('Error', 'Failed to fetch data');
+    }
+  };
+
   const handleSubmit = async () => {
     if (!fromDate || !toDate || !EmployeeId) {
       Alert.alert('Error', 'Please fill all fields');
       return;
     }
-  
+
     const formattedFromDate = moment(fromDate).format('YYYY-MM-DD');
     const formattedToDate = moment(toDate).format('YYYY-MM-DD');
-  
+
     console.log('FromDate:', formattedFromDate);
     console.log('ToDate:', formattedToDate);
     console.log('EmployeeId:', EmployeeId);
-  
+
     try {
       const response = await axios.get(`http://10.0.2.2:3000/api/v2/pen/penalty/${EmployeeId}`, {
         params: {
@@ -118,7 +180,7 @@ const Penalty = ({ navigation }) => {
           Authorization: `Bearer ${token}`,
         },
       });
-  
+
       setData(response.data);
     } catch (error) {
       console.error('Error fetching data:', error.response ? error.response.data : error.message);
@@ -126,157 +188,208 @@ const Penalty = ({ navigation }) => {
     }
   };
 
-  const tableHead = [ 'Date', 'Intime', 'Late Mins', 'Amount'];
+  const tableHead = reportType === 'summary' 
+    ? ['Employee Name', 'Late Mins', 'Amount'] 
+    : reportType === 'other' 
+      ? ['Employee Name', 'Penalty Amount', 'Remarks'] 
+      : ['Date', 'InTime', 'Late Mins', 'Amount'];
 
   const formatTableData = (data) => {
     return data.map((item) => {
-      let formattedInTime = '';
-  
-      if (item.InTime) {
-        try {
-          // Ensure the InTime is treated as UTC before converting to local time
-          formattedInTime = moment.utc(item.InTime).format('HH:mm'); // Convert to local time
-        } catch (error) {
-          console.error('Error formatting InTime:', error);
-          formattedInTime = 'Invalid Time';
+      if (reportType === 'summary') {
+        return [
+          item.EmpName,
+          item.LateMins,
+          item.Amount,
+        ];
+      } else if (reportType === 'other') {
+        return [
+          item.EmpName,
+          item.PenaltyAmount,
+          item.Remarks,
+        ];
+      } else {
+        let formattedInTime = '';
+
+        if (item.InTime) {
+          try {
+            // Ensure the InTime is treated as UTC before converting to local time
+            formattedInTime = moment.utc(item.InTime).format('HH:mm'); // Convert to local time
+          } catch (error) {
+            console.error('Error formatting InTime:', error);
+            formattedInTime = 'Invalid Time';
+          }
         }
+
+        return [
+          moment(item.AttDate).format('DD/MM/YYYY'),
+          formattedInTime,
+          item.LateMins,
+          item.Amount,
+        ];
       }
-  
-      return [
-        moment(item.AttDate).format('DD/MM/YYYY'),
-        formattedInTime,
-        item.LateMins,
-        item.Amount,
-      ];
     });
   };
-  
+
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <View style={styles.header}>
-          <Text style={styles.headerText}>Penalty Report</Text>
+    <ScrollView>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Penalty Report</Text>
+      </View>
+      <View style={styles.filters}>
+        <View style={styles.datePickerContainer}>
+          <TouchableOpacity onPress={() => showDatePicker('from')} style={styles.dateInput}>
+            <Icon name="calendar" size={20} color="#666" style={styles.icon} />
+            <Text style={styles.dateText}>{fromDate ? moment(fromDate).format('DD/MM/YYYY') : 'From Date'}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => showDatePicker('to')} style={styles.dateInput}>
+            <Icon name="calendar" size={20} color="#666" style={styles.icon} />
+            <Text style={styles.dateText}>{toDate ? moment(toDate).format('DD/MM/YYYY') : 'To Date'}</Text>
+          </TouchableOpacity>
         </View>
-        <View style={styles.filters}>
-          <View style={styles.datePickerContainer}>
-            <TouchableOpacity onPress={() => showDatePicker('from')} style={styles.dateInput}>
-              <Icon name="calendar" size={20} color="#666" style={styles.icon} />
-              <Text style={styles.dateText}>{fromDate ? moment(fromDate).format('DD/MM/YYYY') : 'From Date'}</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => showDatePicker('to')} style={styles.dateInput}>
-              <Icon name="calendar" size={20} color="#666" style={styles.icon} />
-              <Text style={styles.dateText}>{toDate ? moment(toDate).format('DD/MM/YYYY') : 'To Date'}</Text>
-            </TouchableOpacity>
-          </View>
-          <Picker
-            selectedValue={reportType}
-            style={styles.input}
-            onValueChange={(itemValue) => setReportType(itemValue)}
-          >
-            <Picker.Item label="Dailywise" value="dailywise" />
-            <Picker.Item label="Summary" value="summary" />
-            <Picker.Item label="Other" value="other" />
-          </Picker>
-          <TextInput
+        <Picker
+          selectedValue={reportType}
+          style={styles.picker}
+          onValueChange={(itemValue) => setReportType(itemValue)}
+        >
+          <Picker.Item label="Daily Wise" value="dailywise" />
+          <Picker.Item label="Summary Wise" value="summary" />
+          <Picker.Item label="Others" value="other" />
+        </Picker>
+        <TextInput
             style={styles.input}
             value={employeeName}
             editable={false}
           />
-          <Button title="Submit" onPress={handleSubmit} />
-        </View>
-        <View style={styles.tableContainer}>
-          <Text style={styles.text}>
+      <TouchableOpacity 
+  style={[styles.button, reportType === 'summary' ? styles.buttonSummary : reportType === 'other' ? styles.buttonOther : styles.buttonDefault]}
+  onPress={reportType === 'summary' ? handleSubmitSummery : reportType === 'other' ? handleSubmitOthers : handleSubmit}
+>
+  <Text style={styles.buttonText}>Generate Report</Text>
+</TouchableOpacity>
+      </View>
+      <View style={styles.tableContainer}>
+      <View style={styles.employeeContainer}>
+      <Text style={styles.employeeLabel}>
             Report from {fromDate ? moment(fromDate).format('DD/MM/YYYY') : 'N/A'} to {toDate ? moment(toDate).format('DD/MM/YYYY') : 'N/A'}
           </Text>
-          <Text style={styles.text}>
-            Employee: {employeeName} 
-          </Text>
-          <Table borderStyle={{ borderWidth: 1, borderColor: '#ccc' }}>
-            <Row data={tableHead} style={styles.head} textStyle={styles.text} />
-            <Rows data={formatTableData(data)} textStyle={styles.text} />
-          </Table>
-        </View>
-        <DateTimePickerModal
-          isVisible={isDatePickerVisible}
-          mode="date"
-          onConfirm={handleConfirm}
-          onCancel={hideDatePicker}
-        />
-      </ScrollView>
-    </SafeAreaView>
-  );
+          </View>
+          <View style={styles.employeeContainer}>
+  <Text style={styles.employeeLabel}>Employee: </Text>
+  <Text style={styles.employeeValue}>{employeeName}</Text>
+</View>
+        <Table borderStyle={{ borderWidth: 1, borderColor: '#c8e1ff' }}>
+          <Row data={tableHead} style={styles.head} textStyle={styles.text} />
+          <Rows data={formatTableData(data)} textStyle={styles.text} />
+        </Table>
+      </View>
+    </ScrollView>
+    <DateTimePickerModal
+      isVisible={isDatePickerVisible}
+      mode="date"
+      onConfirm={handleConfirm}
+      onCancel={hideDatePicker}
+      date={dateMode === 'from' ? fromDate || new Date() : toDate || new Date()}
+    />
+  </SafeAreaView>
+);
 };
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 10,
     backgroundColor: '#f9f9f9',
   },
   header: {
-    justifyContent: 'center',
+    padding: 20,
     alignItems: 'center',
-    marginBottom: 10,
-    paddingHorizontal: 10,
-    backgroundColor: '#4CAF50',
-    paddingVertical: 10,
-    borderRadius: 5,
+    backgroundColor: '#059A5F',
+    
   },
   headerText: {
-    fontSize: 20,
-    fontWeight: 'bold',
+    fontSize: 24,
     color: '#fff',
+    fontWeight: 'bold',
   },
   filters: {
-    marginBottom: 20,
-    padding: 10,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    elevation: 1,
+    padding: 20,
   },
   datePickerContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    marginBottom: 10,
   },
   dateInput: {
+    flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: '#f9f9f9',
+    borderColor: '#ccc',
+    borderWidth: 1,
     padding: 10,
     borderRadius: 5,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    marginVertical: 5,
-    width: '48%',
-  },
-  icon: {
     marginRight: 10,
   },
   dateText: {
-    fontSize: 16,
+    marginLeft: 10,
     color: '#666',
+  },
+  icon: {
+    marginLeft: 10,
   },
   input: {
     height: 40,
     borderColor: '#ccc',
     borderWidth: 1,
     borderRadius: 5,
-    marginVertical: 5,
-    paddingHorizontal: 10,
-    backgroundColor: '#f9f9f9',
-     color: '#666'
+    marginBottom: 10,
+    paddingLeft: 10,
+    color:"black"
   },
   tableContainer: {
-    marginBottom: 20,
+    padding: 20,
   },
   head: {
     height: 40,
     backgroundColor: '#f1f8ff',
+   
   },
   text: {
     margin: 6,
-    textAlign: 'center',
-     color: '#666'
+     color:"black"
+  },
+  employeeContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  },
+  employeeLabel: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginRight: 5,
+  },
+  employeeValue: {
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  button: {
+    backgroundColor: '#059A5F',
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
 
